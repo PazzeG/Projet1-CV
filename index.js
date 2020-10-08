872eca73f5c8333c57e447c3951f3057aa2e06f9
MartyLucas45 <marty45@hotmail.fr>
2020-10-08 12:27:41 +0200

    08/10 Texte Fond OK

--- index.js
+++ index.js
@@ -34,3 +34,72 @@ menuBtn.addEventListener("click", () => {
     menuOpen= false;
   }
 });
+
+
+const fondText = document.querySelector(".backgroundTitle");
+function backgroundText (text){
+    console.log(text.classList);
+    text.addEventListener('mouseenter', function(e){
+        if(text.classList[0] == "diplomes"){
+            fondText.innerHTML = "DIPLOMES";
+        }
+        else if (text.classList[0] == "disponibilite"){
+            fondText.innerHTML = "DISPONIBILITE";
+        }
+        else if (text.classList[0] == "projet"){
+            fondText.innerHTML = "PROJET";
+        }
+        else if (text.classList[0] == "coordonnes"){
+            fondText.innerHTML = "COORDONNES";
+        }
+        else if (text.classList[0] == "competences"){
+            fondText.innerHTML = "COMPETENCES";
+        }
+        else{
+            fondText.innerHTML = "EXPERIENCES";
+        }
+        fondText.style.display = "flex";
+        fondText.animate([
+            // keyframes
+            { opacity: '0%' }, 
+            { opacity: '30%' }
+          ], { 
+            // timing options
+            duration: 500,
+            iterations: 1,
+          });
+          fondText.style.opacity = "30%";
+        text.style.backgroundColor= "rgba(216, 223, 238, 0.9)";
+
+    })
+    text.addEventListener('mouseleave', function(e){
+        fondText.animate([
+            // keyframes
+            { opacity: '30%' }, 
+            { opacity: '0%' }
+          ], { 
+            // timing options
+            duration: 500,
+            iterations: 1,
+          });
+        fondText.style.opacity = "0%";
+        fondText.style.display = "none"; 
+        text.style.background = "";
+    })
+}
+
+const exp = document.querySelector(".experiences");
+const dip = document.querySelector(".diplomes");
+const disp = document.querySelector(".disponibilite");
+const proj = document.querySelector(".projet");
+const coo = document.querySelector(".coordonnes");
+const comp = document.querySelector(".competences");
+backgroundText(exp);
+backgroundText(dip);
+backgroundText(disp);
+backgroundText(proj);
+backgroundText(coo);
+backgroundText(comp);
+
+
+// "#d8dfee";
